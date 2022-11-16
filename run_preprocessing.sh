98887887f58f93de2ad06ee73c2928f2694afbda
Nick <nicholas_petrocelli@brown.edu>
2022-11-16 11:05:59 -0500

    pushing preprocessing to compute cluster

---
+++ run_preprocessing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#  Execute from the current working directory
+# -cwd
+#
+#  This is a long-running job
+# -l inf
+#
+#
+#
+# notify
+# -m abes
+#
+export PATH=/home/npetroce/Desktop/classwork/CS2952C/models-extension/models_env/bin:$PATH
+export DATA_DIR=/home/npetroce/data/
+source models_env/bin/activate
+python3 preprocess_openwebtext.py --data-dir $DATA_DIR 
