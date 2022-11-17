75bd2900fc6eb5bcb1171a31257e55659e1a099e
Nick <nicholas_petrocelli@brown.edu>
2022-11-16 23:08:12 -0500

    no slashes?

--- run_pretraining.py
+++ run_pretraining.py
@@ -141,7 +141,7 @@ def main(data_dir, model_name, model_size, use_pretrained, training_steps):
     checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
     checkpoint_manager = tf.train.CheckpointManager(
         checkpoint,
-        directory=os.path.join(data_dir, 'model_ckpts/', model_name, ''),
+        directory=os.path.join(data_dir, 'model_ckpts', model_name, ''),
         max_to_keep=keep_checkpoint_max,
         step_counter=optimizer.iterations,
         checkpoint_interval=save_checkpoints_steps,
@@ -150,7 +150,7 @@ def main(data_dir, model_name, model_size, use_pretrained, training_steps):
 
     step_count = 0 
     iterator = iter(dataset)
-    csvpath = os.path.join(data_dir, 'model_ckpts/', model_name, 'pretrain_metrics.csv')
+    csvpath = os.path.join(data_dir, 'model_ckpts', model_name, 'pretrain_metrics.csv')
     print(csvpath)
     with open(csvpath, 'w', newline='') as csvfile:
         fieldnames = ['step','total_loss', 'discriminator_loss', 'lm_example_loss', 'effective_masking_rate', 'discriminator_accuracy', 'masked_lm_accuracy']
