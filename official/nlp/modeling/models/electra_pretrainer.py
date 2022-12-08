809c350d549ce7bdab9c9c311bde1dffd87eda21
Nick <nicholas_petrocelli@brown.edu>
2022-12-07 23:13:26 -0500

    fix model signature?

--- official/nlp/modeling/models/electra_pretrainer.py
+++ official/nlp/modeling/models/electra_pretrainer.py
@@ -145,24 +145,17 @@ class ElectraPretrainer(tf.keras.Model):
     input_type_ids = inputs['input_type_ids']
     masked_lm_positions = inputs['masked_lm_positions']
 
-    # slightly different input signatures
-    gen_input = {'input_mask': input_mask, 'input_type_ids': input_type_ids, 'input_word_ids': input_word_ids} if self.use_pretrained_gen else [input_word_ids, input_mask, input_type_ids]
-
-    ### Generator ###
-    sequence_output = self.generator_network(
-        gen_input)['sequence_output']
-    # The generator encoder network may get outputs from all layers.
-    if isinstance(sequence_output, list):
-      sequence_output = sequence_output[-1]
-
-    
     if self.use_pretrained_gen:
-      # slightly different signature
-      # TODO maybe different input signature as well?
-      lm_outputs = self.masked_lm(sequence_output, masked_lm_positions)['mlm_logits']
+      lm_outputs = self.masked_lm(inputs)['mlm_logits']
       # don't bother with sentence outputs
       sentence_outputs = None
     else:
+      ### Generator ###
+      sequence_output = self.generator_network(
+          [input_word_ids, input_mask, input_type_ids])['sequence_output']
+      # The generator encoder network may get outputs from all layers.
+      if isinstance(sequence_output, list):
+        sequence_output = sequence_output[-1]
       lm_outputs = self.masked_lm(sequence_output, masked_lm_positions)
       sentence_outputs = self.classification(sequence_output)
 
