e6f8042245d845a1f0a1191cb1ee17a194eba81f
Nicholas Petrocelli <npetroce@cs.brown.edu>
2022-12-10 00:36:03 -0500

    decent starting value and rate corresponding to 500k steps?

--- run_pretraining.py
+++ run_pretraining.py
@@ -67,7 +67,7 @@ def main(data_dir, model_name, model_size, use_pretrained, training_steps):
     
     max_seq_length = 128
     # TODO doesn't align with paper, need to fit into memory (currently halved)
-    train_batch_size = 1 # TODO int(128 / BATCH_SCALE)
+    train_batch_size = int(128 / BATCH_SCALE)
     eval_batch_size = 128
     # optimization
     learning_rate = 5e-4
@@ -104,8 +104,8 @@ def main(data_dir, model_name, model_size, use_pretrained, training_steps):
             cls_heads=[],
             pretrained_generator=PRETRAINED_MODELS['WRS_BERT_MEDIUM_512_12L'] if use_pretrained else None,
             tie_embeddings=False if use_pretrained else True,
-            mlm_start_temperature=5.0 * pow(10, 7) if use_pretrained else 1.0,
-            mlm_temperature_delta=1.0 * pow(10, 1) if use_pretrained else 0.0),
+            mlm_start_temperature=10.0 * pow(10, 1) if use_pretrained else 1.0,
+            mlm_temperature_delta=2.0 * pow(10, -4) if use_pretrained else 0.0),
         #dummy?
         train_data=pretrain_dataloader.BertPretrainDataConfig(
             tfds_name=None,
